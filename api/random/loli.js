a32766ab20d118b8e233495901d48c72c9847460
arulzxd <arulzz.xd@gmail.com>
2026-07-23 01:28:09 +0700

    Add files via upload

--- api/random/loli.js
+++ api/random/loli.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-// Fungsi untuk mengambil gambar Blue Archive secara acak
-async function bluearchive() {
+// Fungsi untuk mengambil gambar secara acak
+async function loli() {
     try {
-        const { data } = await axios.get(`https://raw.githubusercontent.com/arulzzzxd/database/heads/main/Randombluearchive/randomba.js`);
+        const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/loli-r-img/refs/heads/main/links.json`);
         const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
         return Buffer.from(response.data);
     } catch (error) {
@@ -16,7 +16,7 @@ async function bluearchive() {
 // Endpoint utama Router
 router.get('/', async (req, res) => {
     try {
-        const pedo = await bluearchive();
+        const pedo = await loli();
         res.writeHead(200, {
             'Content-Type': 'image/png',
             'Content-Length': pedo.length,
